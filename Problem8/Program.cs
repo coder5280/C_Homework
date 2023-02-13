ff469ae1853e4b7333e7ef6227786c3267ebf305
azamat Khadikov <khadikovazamat@gmail.com>
2023-02-13 11:45:18 -0700

    Problem8

---
+++ Problem8/Program.cs
@@ -0,0 +1,14 @@
+﻿int N = 5, value = 1;
+while (true)
+{
+    if (value % 2 == 0)
+    {
+        System.Console.WriteLine(value);
+    }
+    if (value >= N)
+    {
+        break;        
+    }    
+    value ++;
+}        
+    
